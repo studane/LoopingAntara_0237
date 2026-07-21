38f62d8f4a721bf335d770431ddbc86ba3d9d8b8
studane <zhidaneprabowoguzlend@gmail.com>
2026-07-21 10:35:38 +0700

    membuat perulangan for

--- LoopingFor.cpp
+++ LoopingFor.cpp
@@ -2,5 +2,14 @@
 using namespace std;
 
 int main(){
+    //Perulangan dengan FOR
+    cout << "PERULANGAN FOR" << endl;
+
+    //Perulangan dengan FOR Naik
+    for (int i = 0; i < 5; i++) {
+        cout << "Nilai i : " << i << " Teknologi Informasi UMY" << endl;
+    }
+    cout << endl;
+
 
 }
